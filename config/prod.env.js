485ce4989bf630a5f37a386e9c62fb1c270a017c
GavinKim <dreamworld612@gmail.com>
2018-07-29 11:20:46 +0900

    modify prod.env

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API: '"http://localhost:5959"' //FIXME: 최종 라이브 버전 정보로 업데이트 할것
+  API: '"http://52.231.154.198:5959"' //FIXME: 최종 라이브 버전 정보로 업데이트 할것
 }
